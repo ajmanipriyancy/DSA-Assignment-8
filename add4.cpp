a5ae0ebf07b10581fb1b210739c8119ee7e896d9
ajmanipriyancy <ajmanipriyancy@gmail.com>
2025-11-28 23:54:09 +0530

    Create add4.cpp

---
+++ add4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+vector<int> rightSideView(TreeNode* root) {
+    vector<int> res;
+    if (!root) return res;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        int sz = q.size();
+        for (int i = 0; i < sz; i++) {
+            TreeNode* cur = q.front();
+            q.pop();
+            if (i == sz - 1) res.push_back(cur->val);
+            if (cur->left) q.push(cur->left);
+            if (cur->right) q.push(cur->right);
+        }
+    }
+    return res;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(4);
+
+    vector<int> ans = rightSideView(root);
+    for (int x : ans) cout << x << " ";
+}
