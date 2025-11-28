2fabc4c6cd00d59cf47f8a5ed66e91c1750216ab
ajmanipriyancy <ajmanipriyancy@gmail.com>
2025-11-28 23:53:28 +0530

    Create add3.cpp

---
+++ add3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if (!root) return 0;
+    int l = maxDepth(root->left);
+    int r = maxDepth(root->right);
+    return 1 + (l > r ? l : r);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    cout << maxDepth(root);
+}
